9e2a61f7bcbab8d963b5b8a61c2b3b387cf7ca8d
Matheus Icaro <matheusicaro2@hotmail.com>
2018-09-29 11:10:01 -0300

    listing elements in view.

--- react-fundamentals/lesson-3/reactnd-contacts-app/src/components/ListContacts.js
+++ react-fundamentals/lesson-3/reactnd-contacts-app/src/components/ListContacts.js
@@ -5,12 +5,18 @@ class ListContacts extends Component {
     render(){
 
         console.log("props..:", this.props);
-
+        
         return (
             <ol className="list-contacts">
-
+                {   this.props.contacts.map(element =>(
+                        <li key={ element.id }>
+                            {element.name}
+                        </li>
+                    ))
+                }
             </ol>
         )
+
     }
 }
 export default ListContacts
