e714e1ca7b50a305fd64dfda0bcbb79bf40c3d19
Matheus Icaro <matheusicaro2@hotmail.com>
2018-09-29 11:45:30 -0300

    inserting tags and style for elements in view.

--- react-fundamentals/lesson-3/reactnd-contacts-app/src/components/ListContacts.js
+++ react-fundamentals/lesson-3/reactnd-contacts-app/src/components/ListContacts.js
@@ -8,15 +8,32 @@ class ListContacts extends Component {
         
         return (
             <ol className="list-contacts">
-                {   this.props.contacts.map(element =>(
-                        <li key={ element.id }>
-                            {element.name}
+                {
+                    this.props.contacts.map( (element) =>(
+
+                        <li key={ element.id } className='contact-list-item'>
+                            
+                            <div 
+                                className='contact-avatar'
+                                style={{
+                                    backgroundImage: `url( ${element.avatarURL} )`
+                                }}
+                            ></div>
+                            
+                            <div className='contact-details'>
+                                <p>{ element.name }</p>
+                                <p>@{ element.handle }</p>
+                            </div>
+
+                            <div className='contact-remove'>
+                                remove                                
+                            </div>
+                            
                         </li>
                     ))
                 }
             </ol>
         )
-
     }
 }
 export default ListContacts
