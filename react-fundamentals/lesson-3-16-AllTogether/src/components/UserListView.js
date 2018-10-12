d55bb21fc1864f3fa7e709aa4beaf34651326344
Matheus Icaro <matheusicaro2@hotmail.com>
2018-10-12 16:19:56 -0300

    add button and views for games played.

--- react-fundamentals/lesson-3-16-AllTogether/src/components/UserListView.js
+++ react-fundamentals/lesson-3-16-AllTogether/src/components/UserListView.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react';
 import '../styles/style.css';
 
-const UserListView = (props)=>{
+class UserListView extends Component{
 
-    return(
-        <div className="Component">
-            
-            <ol>
-                { props.users.map(element =>(
-                    <li>
-                        {`${ element.firstName } - ${ element.lastName } - ${ element.login }`}
-                    </li>
-                ))}
-            </ol>
+    state = {
+        isToShow: false
+    }
+
+    hideOrShow = () => {
+        this.setState(currentState =>({
+            isToShow: !currentState.isToShow
+        })) 
+    }
+
+    buttonShowHide = (
+        <div>
+            <button className="smallButton" onClick={ this.hideOrShow }>
+                Hide nº of games
+
+            </button>
         </div>
     )
+
+    render(){
+
+        const {isToShow} = this.state;
+        const {users} = this.props;
+
+        return(
+            <div className="Component">                
+                
+                { (users && users.length > 0) ? this.buttonShowHide : (<h2> 0 - items... </h2>)}   
+
+                <ol>
+                    { users.map(user =>(
+                        <li key={user.firstName} >
+                            {`${ user.firstName } ${ user.lastName } - `} 
+                            {(isToShow) ? '(Hide the Number of Games Played)' : (`played ${user.numberOfGames} games`) }
+                        </li>
+                    ))}
+                </ol>
+
+            </div>
+        )
+    }
 }
 
 export default UserListView 
