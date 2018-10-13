b818650cb35d4146ad945a577279b9e3a1d461fd
Matheus Icaro <matheusicaro2@hotmail.com>
2018-10-13 17:40:16 -0300

    introducing ajax requests obeying the React standards using the componentDidMount () method.

--- react-fundamentals/lesson-3/lesson initial/reactnd-contacts-app/src/App.js
+++ react-fundamentals/lesson-3/lesson initial/reactnd-contacts-app/src/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
 import ListContacts from './components/ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
+  }
+  
+  componentDidMount() {
+    
+      ContactsAPI.getAll().then((contacts) => {
+      
+      this.setState(() => ({
+        contacts
+      }))
+    })
   }
   
   removeContac = (contact)=> {
@@ -34,6 +26,7 @@ class App extends Component {
       })
 
     }))
+    ContactsAPI.remove(contact)
   }
 
   render() {
